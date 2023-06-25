e9d9f357733f1ae64fd92fe31eecb2042207ec56
Padidar <Padidar@DESKTOP-NA8IQP4>
2023-06-25 09:31:29 +0330

    Add project files.

---
+++ ConsoleApp5/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Project
+{
+    class Test1
+    {
+        public static void Main()
+        {
+            Console.WriteLine("Enter x:");
+            int x = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter x:");
+            int y = Convert.ToInt32(Console.ReadLine());
+
+            Calc(x, y);
+        }
+        private static void Calc(int x, int y)
+        {
+            double result = Math.Sqrt(Math.Abs(x) + Math.Pow(y, 2));
+
+            Console.WriteLine("\nResualt is: " + result);
+        }
+    }
+}
